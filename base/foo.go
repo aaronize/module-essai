dca8fe2291b6ec1f5365356e444e90508f3bd6dc
aaron chen <aaron.chen@ucloud.cn>
2020-01-19 11:27:49 +0800

    modify

--- base/foo.go
+++ base/foo.go
@@ -2,8 +2,14 @@ package base
 
 import "fmt"
 
-func Func() {
-	fmt.Println("func version 1")
+// func Func() {
+// 	fmt.Println("func version 1")
+
+// 	return
+// }
+
+func Func2() {
+	fmt.Println("func version 2")
 
 	return
 }
